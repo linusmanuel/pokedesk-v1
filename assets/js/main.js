641e7f230fd9d97724e28de9c80bb34c59c74408
Joao Lino <linusmannuel@gmail.com>
2024-02-05 05:50:47 +0100

    feature: converting pokeapi model to our model

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,18 @@
-// import { pokeapi } from './poke-api.js';
+import { pokeapi } from './poke-api.js';
 const pokemonOl = document.querySelector('.pokemons');
 
 function convertPokemonToLi(pokemon) {
 	return `
-		<li class="pokemon">
-			<span class="number">${pokemon.order}</span>
+		<li class="pokemon ${pokemon.type}">
+			<span class="number">${pokemon.number}</span>
 			<span class="name">${pokemon.name}</span>
 
 			<div class="detail">
 				<ol class="types">
-					<li class="type">grass</li>
-					<li class="type">poison</li>
+					${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
 				</ol>
 
-				<img
-					src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-					alt=${pokemon.name}
-				/>
+				<img src=${pokemon.photo} alt=${pokemon.name} />
 			</div>
 		</li>
 	`;
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,24 @@
-const pokeapi = {};
+import { Pokemon } from './pokemon-model.js';
+export const pokeapi = {};
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+	const pokemon = new Pokemon();
+	pokemon.number = pokeDetail.order;
+	pokemon.name = pokeDetail.name;
+
+	const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+	const [type] = types;
+	pokemon.types = types;
+	pokemon.type = type;
+	pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+	return pokemon;
+}
 
 pokeapi.getPokemonDetail = (pokemon) => {
-	return fetch(pokemon.url).then((response) => response.json());
+	return fetch(pokemon.url)
+		.then((response) => response.json())
+		.then(convertPokeApiDetailToPokemon);
 };
 
 pokeapi.getPokemons = (offset = 0, limit = 5) => {
